ab3cd7952194691dcb3742a075b651ff6b3ff26f
panos1962 <panos1962@gmail.com>
2019-12-08 22:20:57 +0200

    modifications

--- bin/carget.js
+++ bin/carget.js
@@ -11,13 +11,33 @@ const readline = require('readline');
 const pd = require(`${process.env.PANDORA_BASEDIR}/lib/pandora.js`);
 const govHUB = require(pd.env('CHT_BASEDIR', '/var/opt/cht') + '/lib/govHUB/api.js');
 
+//pd.ttymsg = pd.noop;
+
+function errorPrint(msg) {
+	pd.errmsg(msg);
+	process.stdout.write("ERROR\n");
+}
+
 govHUB.confRead(`${process.env.CHT_BASEDIR}/private/govHUB.cf`, (cfs) => {
 	govHUB.tokenGet(JSON.parse('{' + cfs + '}'), (conf) => {
 		pd.ttymsg('Reading cars...\n');
-		readline.createInterface({
+		const rl = readline.createInterface({
 			'input': process.stdin,
+			'crlfDelay': Infinity,
 		}).on('line', (line) => {
-			govHUB.carGet(line, conf);
+			try {
+				var data = JSON.parse(line);
+			} catch (e) {
+				return errorPrint(line + ": syntax error");
+			}
+
+			if (!data.hasOwnProperty('oxima'))
+			return errorPrint(line + ': missing "oxima" property');
+
+
+			govHUB.carGet(data, conf, (oxima) => {
+				console.log(JSON.stringify(oxima));
+			});
 		});
 	});
 });

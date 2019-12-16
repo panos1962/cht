c8970cf315bfa22e8be56c0a04f9b2408a7d0b39
panos1962 <panos1962@gmail.com>
2019-12-16 15:35:43 +0200

    modifications

--- tutorial/govHUB/carget/test.sh
+++ tutorial/govHUB/carget/test.sh
@@ -20,8 +20,10 @@
 CHT_BASEDIR="/var/opt/cht"
 
 echo "Default:" >/dev/tty
-#"${CHT_BASEDIR}/bin/GH" carget default[12].data
+#"${CHT_BASEDIR}/bin/GH" carget -v default[12].data
 
-echo "Custom:" >/dev/tty
-#"${CHT_BASEDIR}/bin/GH" carget -s test1.js test1[12].data
+echo "Custom (test1):" >/dev/tty
+"${CHT_BASEDIR}/bin/GH" carget -v -s test1.js test1[12].data
+
+echo "Custom (test2):" >/dev/tty
 "${CHT_BASEDIR}/bin/GH" carget -v -s test2.js test2[2].data

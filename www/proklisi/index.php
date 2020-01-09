5f7dde92e0b649c6ef089a10ce42e52dab3ea8ea
panos1962 <panos1962@gmail.com>
2020-01-09 12:02:19 +0200

    modifications

---
+++ www/proklisi/index.php
@@ -0,0 +1,37 @@
+<!--
+###############################################################################@
+##
+##
+## @BEGIN
+##
+## @COPYRIGHT BEGIN
+## Copyright (C) 2020 Panos I. Papadopoulos <panos1962_AT_gmail_DOT_com>
+## @COPYRIGHT END
+##
+## @FILETYPE BEGIN
+## php
+## @FILETYPE END
+##
+## @FILE BEGIN
+## www/proklisi/index.php —— Βασική σελίδα διαχείρισης προ-κλήσεων.
+## @FILE END
+##
+## @DESCRIPTION BEGIN
+## Πρόκειται για μενού βασικών επιλογών διαχείρισης προ-κλήσεων.
+## @DESCRIPTION END
+##
+## @HISTORY BEGIN
+## Created: 2020-01-09
+## @HISTORY END
+##
+## @END
+##
+###############################################################################@
+-->
+
+<html>
+<head>
+</head>
+<body>
+</body>
+</html>

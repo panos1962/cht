652f1f8b47a862502aef076878d0c2db2262d8ef
panos1962 <panos1962@gmail.com>
2020-01-11 16:42:40 +0200

    modifications

--- www/govHUB/index.php
+++ www/govHUB/index.php
@@ -62,7 +62,9 @@ Pandora::import_php([
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
 <link rel="stylesheet" href="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/themes/smoothness/jquery-ui.css">
 <script src="https://ajax.googleapis.com/ajax/libs/jqueryui/1.12.1/jquery-ui.min.js"></script>
+<script>console.log('>>>', 'govHUB/index.php (before bundle.js)');</script>
 <script src="bundle.js"></script>
+<script>console.log('>>>', 'govHUB/index.php (after bundle.js)');</script>
 </head>
 
 <body>

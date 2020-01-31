9e47ade689d6d55118bc183e770f3bc13101c144
panos1962 <panos1962@gmail.com>
2020-01-31 19:56:09 +0200

    modifications

--- www/govHUB/ektiposi.php
+++ www/govHUB/ektiposi.php
@@ -1,9 +1,28 @@
 <?php
 ///////////////////////////////////////////////////////////////////////////////@
 //
-// Copyright (C) 2020 Panos I. Papadopoulos <panos1962_AT_gmail_DOT_com>
+// @BEGIN
 //
+// @COPYRIGHT BEGIN
+// Copyright (C) 2020 Panos I. Papadopoulos <panos1962_AT_gmail_DOT_com>
+// @COPYRIGHT END
+//
+// @FILETYPE BEGIN
+// javascipt
+// @FILETYPE END
+//
+// @FILE BEGIN
+// www/govHUB/ektiposi/ektiposi.js —— Πρόγραμμα οδήγησης σελίδας εκτύπωσης
+// αποτελεσμάτων αναζήτησης στοιχείων οχημάτων, κατόχων οχημάτων, φυσικών
+// και νομικών προσώπων μέσω της πλατφόρμας "govHUB".
+// @FILE END
+//
+// @HISTORY BEGIN
+// Updated: 2020-01-31
 // Created: 2020-01-05
+// @HISTORY END
+//
+// @END
 //
 ///////////////////////////////////////////////////////////////////////////////@
 
